615a9c2d7a8c3fe5e5f3e7c7d8e66aaa20f2bc22
Peter T. Rühr <peter.ruehr@gmail.com>
2022-04-17 00:55:48 +0200

    Update piscAnt.py

--- piscAnt.py
+++ piscAnt.py
@@ -1,4 +1,4 @@
-# v.0.0.9003
+# v.0.0.9004
 
 from time import sleep
 from time import strftime
@@ -66,7 +66,6 @@ def move_motor_by_steps(axis, by_steps):
     
     target = curr_position + by_steps
     
-    # ~ print('moving ' + axis + '-axis by ' + str(by_steps) + '...')
     print('moving ' + axis + '-axis by ' + str(by_steps) + ' to ' + str(target) + '...')
     
     cmd = 'ticcmd --exit-safe-start -d ' + curr_axis_ID + ' --position ' + str(target)
@@ -97,22 +96,25 @@ def deenergize():
     os.system(cmd)
        
 def move_motor_to(axis, target):
-    curr_axis_ID = get_motor_ID(axis)
         
     curr_position = get_motor_pos(axis)
     # print(axis + '-axis now at ' + str(curr_position))
     
     by_steps = target - curr_position
     
-    print('moving ' + axis + '-axis by ' + str(by_steps) + ' to ' + str(target) + '...')
+    move_motor_by_steps(axis, by_steps)
     
-    cmd = 'ticcmd --exit-safe-start -d ' + curr_axis_ID + ' --position ' + str(target)
-    os.system(cmd)
-    
+    # ~ cm = get_motor_pos(axis)
+    # ~ print(cm)
+    # ~ while(cm != target):
+        # ~ move_motor_by_steps(axis, by_steps)
+        # ~ cm = get_motor_pos(axis)
+        # ~ print(cm)
+        # ~ sleep(5)
     # sleep shortly to read correct number
-    # sleep(0.25)
-    # get_motor_ID(axis)
-    # print(axis + '-axis now at ' + str(get_motor_pos(curr_axis_ID)))
+    sleep(abs(round(by_steps/2000, 2)))
+    get_motor_ID(axis)
+    print(axis + '-axis now at ' + str(get_motor_pos(axis)))
 
 def shoot_image_scan(x, y, z):
     # ~ x_pos = get_motor_pos('x')
@@ -121,7 +123,7 @@ def shoot_image_scan(x, y, z):
     sleep(0.5)
     curr_image_name = './img_02/x_' + str(x) + '_y_' + str(y) + '_z_' + str(z)  + '.jpg'
     # curr_image_name = './img_01/x_' + str(x_pos) + '_y_' + str(y_pos) + '_z_' + str(z_pos)  + '.jpg'
-    print('Saving ' + curr_image_name + '...')
+    # print('Saving ' + curr_image_name + '...')
     cmd = 'raspistill -t 1 -o ' + curr_image_name + ' -n -ISO 100, -sh 0, -co 0, -br 50, -sa 0'
     os.system(cmd)
     
@@ -148,12 +150,12 @@ def start_scan():
     # arm
     x_min = -150
     x_max = 110
-    positions_x = 6
+    positions_x = 5
     steps_x = round((x_max - x_min) / positions_x)
     print('steps_x = ' + str(steps_x))
 
     # turntable
-    y_min = get_motor_pos('y')
+    y_min = 0 # + get_motor_pos('y')
     y_max = y_min + 1600
     positions_y = 20
     steps_y = round((y_max - y_min)/positions_y)
@@ -162,11 +164,12 @@ def start_scan():
     #focus
     z_min = -2300
     z_max = 5500
-    positions_z = 50
+    positions_z = 30
     steps_z = round((z_max - z_min)/positions_z)
     print('steps_z = ' + str(steps_z))
     
-    print('Making ' + str(steps_x*steps_y*steps_z) + ' photos...')
+    print('Making ' + str(positions_x*positions_y*(positions_z+5)) + ' photos,')
+    print('resulting in ~' + str((positions_x*positions_y*(positions_z)/1024)) + ' GB.')
     
     x_pos_init = x_min
     y_pos_init = y_min
@@ -177,6 +180,7 @@ def start_scan():
     move_motor_to('x', x_min)
     move_motor_to('y', y_min)
     move_motor_to('z', z_min)
+    sleep(2)
     
     for x in range(positions_x):
         for y in range(positions_y):
@@ -188,8 +192,7 @@ def start_scan():
                     z += 1
                     shoot_image_scan(x, y, z)
                     
-                    move_motor_to('z', z_pos_init) 
-            
+                    move_motor_to('z', z_pos_init)
             move_motor_by_steps('y', steps_y)
             if(y == positions_y-1): 
                 y += 1   
@@ -202,11 +205,11 @@ def start_scan():
                         z += 1
                         shoot_image_scan(x, y, z)
                         
-                        move_motor_to('z', z_pos_init)             
-                move_motor_to('y', y_pos_init)
+                        move_motor_to('z', z_pos_init)
+                # move_motor_to('y', y_pos_init)
         move_motor_by_steps('x', steps_x)
         sleep(1)
-        if(x == positions_x):
+        if(x == positions_x-1):
             x += 1
             for y in range(positions_y):
                 for z in range(positions_z):
@@ -214,11 +217,11 @@ def start_scan():
                   
                     move_motor_by_steps('z', steps_z)  
                 
-                if(z == positions_z-1):
-                    z += 1
-                    shoot_image_scan(x, y, z)
-                    
-                    move_motor_to('z', z_pos_init) 
+                    if(z == positions_z-1):
+                        z += 1
+                        shoot_image_scan(x, y, z)
+                        
+                        move_motor_to('z', z_pos_init)
             
                 move_motor_by_steps('y', steps_y)
                 if(y == positions_y-1):  
@@ -232,8 +235,8 @@ def start_scan():
                             z += 1
                             shoot_image_scan(x, y, z)
                             
-                            move_motor_to('z', z_pos_init)             
-                    move_motor_to('y', y_pos_init)
+                            move_motor_to('z', z_pos_init)
+                    # move_motor_to('y', y_pos_init)
     print('done')
 
 
@@ -306,10 +309,10 @@ focus_right_sm = tk.Button(root, text='focus further >>',
 width=15, state='normal', command=lambda: move_motor_by_steps('z', -100))
 
 focus_left_big = tk.Button(root, text='<< focus closer',  
-width=15, state='normal', command=lambda: move_motor_by_steps('z', +500))
+width=15, state='normal', command=lambda: move_motor_by_steps('z', +5000))
 
 focus_right_big = tk.Button(root, text='focus further >>',  
-width=15, state='normal', command=lambda: move_motor_by_steps('z', -500))
+width=15, state='normal', command=lambda: move_motor_by_steps('z', -5000))
 
 start_scan_b = tk.Button(root, text='Start Scan!',  
 width=15, state='normal', command=lambda: start_scan())
@@ -353,5 +356,3 @@ start_scan_b.grid(row=r,column=0, columnspan = 2)
 # fan_off['state']='disabled'
 # pump_off['state']='disabled'
 root.mainloop()
-
-
